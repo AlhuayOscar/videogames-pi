8d5927231397cc25e7857bc58eac05933c44c3d7
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-02-13 21:07:31 -0300

    Modelos de Genero y Videogame, Relaciones y Tabla de generos a la base de datos.

--- api/src/models/genre.js
+++ api/src/models/genre.js
@@ -2,7 +2,6 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo correspondiente a la tabla genre
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('genre', {
       name: {
           type: DataTypes.STRING
