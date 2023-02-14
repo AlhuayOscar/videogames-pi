80a3371811cfd21156be193735c7e735b8dab8e4
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-02-13 21:06:43 -0300

    Modelos de Genero y Videogame, Relaciones y Tabla de generos a la base de datos.

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,10 +1,29 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define("videogame", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allownull: false,
+      primaryKey: true,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    reldate: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.DECIMAL,
+    },
+    platform: {
       type: DataTypes.STRING,
       allowNull: false,
     },
