f60ca359175558ab878cb89a40595969bce25fff
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-02-13 23:02:19 -0300

    Rutas y Redux hechos por ahora

--- api/src/routes/platforms.js
+++ api/src/routes/platforms.js
@@ -1,12 +1,14 @@
-var express = require('express');
-const {apikey} = require('../db');
+var express = require("express");
+const { apikey } = require("../db");
 var router = express.Router();
-const axios = require('axios');
+const axios = require("axios");
 
-//Search all videogames platforms
-router.get('/', async (req, res) => {  
-    var apiresult = await axios.get(`https://api.rawg.io/api/platforms/lists/parents?key=${apikey}`)
-    var apivgplat = apiresult.data.results.map(p => p.name)
-    res.send(apivgplat)
-})    
+router.get("/", async (req, res) => {
+  //Ruta para las plataformas disponibles:
+  var apiresult = await axios.get(
+    `https://api.rawg.io/api/platforms/lists/parents?key=${apikey}`
+  );
+  var apivgplat = apiresult.data.results.map((p) => p.name);
+  res.send(apivgplat);
+});
 module.exports = router;
