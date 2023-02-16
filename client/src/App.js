c52551643a6bb36d1911424c604d31efe511875c
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-02-15 23:23:40 -0300

    Working Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import LandingPage from './components/LandingPage/LandingPage'
+import HomePage from './components/HomePage/HomePage'
+import AddVideogame from './components/AddVideogame/AddVideogame'
+import VideoGameDetails from './components/VideoGameDetails/VideoGameDetails'
+ 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Videogames</h1>
-    </div>
+    <BrowserRouter> 
+    <div className='App'>
+      <Switch>
+        <Route exact path= '/' component = {LandingPage} />
+        <Route exact path= '/videogame' component = {AddVideogame} />
+        <Route exact path= '/videogame/:id' component = {VideoGameDetails} />
+        <Route exact path = '/home' component = {HomePage} />
+        <Route path="*" component={LandingPage} />
+      </Switch>
+    </div>  
+    </BrowserRouter>
   );
 }
 
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { MemoryRouter } from "react-router-dom";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+import HomePage from "./components/HomePage/HomePage";
+import LandingPage from "./components/LandingPage/LandingPage";
+//import TodoDetail from './components/TodoDetail/TodoDetail';
+
+//import Nav from './components/Nav/Nav';
+
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  let store;
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
+
+  beforeEach(() => {
+    store = mockStore([]);
+  });
+
+  describe("El componente Nav debe renderizar en todas las rutas.", () => {
+    it('Debería renderizarse en la ruta "/"', () => {
+      const wrapper = mount(
+        <Provider store={store}>
+          <MemoryRouter initialEntries={["/"]}>
+            <App />
+          </MemoryRouter>
+        </Provider>
+      );
+      expect(wrapper.find(Nav)).toHaveLength(1);
+    });
+  });
+
+
 });
